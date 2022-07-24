824bac572948683688c762bc80a047573a41dc5f
kitten1139 <106716550+kitten1139@users.noreply.github.com>
2022-07-24 14:50:21 +0000

    アラートメッセージとエラーの表示を追加しました

--- sample.js
+++ sample.js
@@ -1,5 +1,9 @@
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)) {
+    alert('グー・チョキ・パーのいずれかを入力してください');
+    user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
 // じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
@@ -7,9 +11,13 @@ let js_hand = getJShand();
 // ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge = winLose(user_hand, js_hand);
 
-// 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+if (user_hand != null) {
 
+// 結果を表示する
+    alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+    alert('またチャレンジしてね');
+}
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
